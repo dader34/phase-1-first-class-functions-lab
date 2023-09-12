47b1955bdbfc337b6164ca6e4e5064227c1d4266
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-09-11 21:12:35 -0700

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => [arr[0],arr[1]]
+const returnLastTwoDrivers = (arr) => [arr[arr.length-2],arr[arr.length-1]]
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+const createFareMultiplier = (mult) =>{
+    return (fare) =>{
+        return mult * fare
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+const selectDifferentDrivers = (arrayOfDrivers,callback) =>{
+    return callback(arrayOfDrivers)
+}
